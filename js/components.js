7d609cdf89e7866559e50f2059634c6373a36e19
User <user@Admin-MacBook-Air.local>
2016-10-10 23:10:04 +0300

    Refactor

---
+++ js/components.js
@@ -0,0 +1,87 @@
+var Game = function () {
+  // disable ipad scrolling
+  document.addEventListener('touchmove',function(event){event.preventDefault();},false);
+
+  // elements to variables
+  this.gameElem = document.getElementById('game');
+  this.timeElem = document.getElementById('time');
+  this.canvasElem = document.getElementById('canvas');
+  this.ctx = this.canvasElem.getContext("2d");
+
+  this.timeCounter = 0;
+  this.pointCounter = 0;
+
+  // attach fastclick
+  FastClick.attach(this.canvasElem);
+}
+
+var Player = function (posOnX,posOnY,height,width) {
+  this.positionOnX = posOnX;
+  this.positionOnY = posOnY;
+  this.height = height;
+  this.width = width;
+
+  this.maxPositionOnY = 298;
+  this.goingUp = false;
+};
+Player.prototype.move = function () {
+  // move inside canvas
+  if ((this.goingUp) && (this.positionOnY>0)){
+    this.positionOnY -= 2;
+  } else if ((!this.goingUp) && (this.positionOnY<this.maxPositionOnY)) {
+    this.positionOnY += 2;
+  }
+};
+Player.prototype.draw = function () {
+  // draw
+  game.ctx.fillStyle='blue';
+  game.ctx.fillRect(this.positionOnX,this.positionOnY,this.height,this.width);
+};
+
+var Foe = function (posOnX,posOnY,height,width) {
+  this.positionOnX = posOnX;
+  this.positionOnY = posOnY;
+  this.height = height;
+  this.width = width;
+
+};
+Foe.prototype.reset = function () {
+  this.positionOnX= 488;
+  this.positionOnY = getRandom(0,308);
+}
+Foe.prototype.move = function () {
+  // enemy too far left
+  if (this.positionOnX<-this.width){
+    this.reset();
+  }
+  this.positionOnX -= 5;
+}
+Foe.prototype.draw = function () {
+  // draw
+  game.ctx.fillStyle='red';
+  game.ctx.fillRect(this.positionOnX,this.positionOnY,this.height,this.width);
+};
+
+
+var Collectable = function (posOnX,posOnY,height,width) {
+  this.positionOnX = posOnX;
+  this.positionOnY = posOnY;
+  this.height = height;
+  this.width = width;
+};
+Collectable.prototype.reset = function () {
+  this.positionOnX = 488;
+  this.positionOnY = getRandom(0,290);
+}
+Collectable.prototype.move = function () {
+  // point too far left or collected
+  if(this.positionOnX<-this.width){
+    this.reset();
+  }
+  this.positionOnX -= 4;
+}
+Collectable.prototype.draw = function () {
+  // draw
+  game.ctx.fillStyle='orange';
+  game.ctx.fillRect(this.positionOnX,this.positionOnY,this.height,this.width);
+};
--- js/neli.js
+++ js/neli.js
@@ -1,90 +1,65 @@
-function updateProgress() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawMe();
+Neli = function () {
+  game = new Game();
+  player = new Player(35,100,20,20);
+  foe = new Foe(435,110,10,10);
+  collectable = new Collectable(435,100,10,10);
 
-  // point hit
-  if ((me[0]<(point[0]+objectWidth))&&(point[0]<(me[0]+myWidth))&&(me[1]<(point[1]+objectHeight))&&(point[1]<(me[1]+myHeight))){
-    points++;
-    //helpElem.innerHTML = points;
-    drawPoint('collected');
-  } else {
-    drawPoint();
-  }
 
-  // enemy hit
-  if ((me[0]<(enemy[0]+objectWidth))&&(enemy[0]<(me[0]+myWidth))&&(me[1]<(enemy[1]+objectHeight))&&(enemy[1]<(me[1]+myHeight))){
-    gameOver();
-  } else {
-    drawEnemy();
-    timeCounter++;
-    //timeElem.innerHTML = enemyLevel + ': ' + timeCounter;
-    requestAnimationFrame(updateProgress);
-  }
-}
+  // states
 
-function drawMe() {
-  // move inside canvas
-  if ((meGoingUp) && (me[1]>0)){
-    me[1] -= 2;
-  } else if ((!meGoingUp) && (me[1]<meMaxBottom)) {
-    me[1] += 2;
-  }
-  
-  // draw
-  ctx.fillStyle='blue';
-  ctx.fillRect(me[0],me[1],me[2],me[3]);
-}
+  updateState = function () {
+    game.ctx.clearRect(0, 0, game.canvasElem.width, game.canvasElem.height);
+    player.move();
+    player.draw();
 
-function drawEnemy() {
-  // enemy too far left
-  if (enemy[0]<-objectWidth){
-    enemy[0] = 488;
-    enemy[1] = getRandom(0,308);
-    // level up
-    if(enemyTimeCounter>500){
-      enemyLevel++;
-      enemyTimeCounter = 0;
+    // collectable
+    if ((player.positionOnX<(collectable.positionOnX+collectable.width))&&(collectable.positionOnX<(player.positionOnX+player.width))&&(player.positionOnY<(collectable.positionOnY+collectable.height))&&(collectable.positionOnY<(player.positionOnY+player.height))){
+      game.pointCounter++;
+      player.width++;
+      player.height++;
+      collectable.reset();
+    } else {
+      collectable.move();
+      collectable.draw();
     }
-    enemyMoveCounter = 0;
-    myPosition = me[1];
-  }
-  // move enemy towards position of own block
-  if ((enemyMoveCounter*enemyLevel)<400){
-    if ((myPosition+10)<enemy[1]){
-      enemy[1] -= (enemyLevel-1);
-    } else if ((myPosition-10)>enemy[1]) {
-      enemy[1] += (enemyLevel-1);
+
+    // enemy hit
+    if ((player.positionOnX<(foe.positionOnX+foe.width))&&(foe.positionOnX<(player.positionOnX+player.width))&&(player.positionOnY<(foe.positionOnY+foe.height))&&(foe.positionOnY<(player.positionOnY+player.height))){
+      gameOver();
+    } else {
+      foe.move();
+      foe.draw();
+      game.timeCounter++;
+      //timeElem.innerHTML = enemyLevel + ': ' + timeCounter;
+      requestAnimationFrame(updateState);
     }
-    enemyMoveCounter++;
   }
-  enemy[0] -= enemyLevel;
-  
-  // draw
-  ctx.fillStyle='red';
-  ctx.fillRect(enemy[0],enemy[1],enemy[2],enemy[3]);
-  
-  enemyTimeCounter++;
-}
 
-function drawPoint(state) {
-  // point too far left or collected
-  if((point[0]<-objectWidth)||(state=='collected')){
-    point[0] = 488;
-    point[1] = getRandom(0,290);
+  gameOver = function () {
+    game.gameElem.innerHTML = 'Points collected: '+game.pointCounter;
   }
-  point[0] -= 4;
-  
-  // draw
-  ctx.fillStyle='orange';
-  ctx.fillRect(point[0],point[1],point[2],point[3]);
-}
 
-function gameOver() {
-  menuElem.style.display = 'block';
-  gameElem.style.display = 'none';
-  prevPointsElem.innerHTML = 'Points collected: '+points;
-}
 
-function getRandom(min, max) {
-  return Math.random() * (max - min) + min;
+  // events
+
+  game.canvasElem.ontouchstart=function(){
+    player.goingUp = true;
+  };
+
+  game.canvasElem.onmousedown=function(){
+    player.goingUp = true;
+  };
+
+  game.canvasElem.ontouchend=function(){
+    player.goingUp = false;
+  };
+
+  game.canvasElem.onmouseup=function(){
+    player.goingUp = false;
+  };
+
+
+  // start running the game
+
+  updateState();
 }
